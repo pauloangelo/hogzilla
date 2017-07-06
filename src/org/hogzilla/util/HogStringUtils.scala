def71417aefbe39b3e3536ba53bcb2f5f0627f15
Paulo Angelo <pa@pauloangelo.com>
2017-07-06 10:07:08 -0300

    Two new auth methods and some bugs

---
+++ src/org/hogzilla/util/HogStringUtils.scala
@@ -0,0 +1,36 @@
+/*
+* Copyright (C) 2015-2016 Paulo Angelo Alves Resende <pa@pauloangelo.com>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License Version 2 as
+* published by the Free Software Foundation.  You may not use, modify or
+* distribute this program under any other version of the GNU General
+* Public License.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+package org.hogzilla.util
+
+import java.security.MessageDigest
+
+import org.apache.hadoop.hbase.util.Bytes
+
+
+/**
+ * @author pa
+ */
+object HogStringUtils {
+  
+  def md5(string:String):String =
+  {
+    Bytes.toString(MessageDigest.getInstance("MD5").digest(string.getBytes))  
+  }
+}
